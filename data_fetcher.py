3c6df6c11458409f53ffb97f25f18d020764cc91
Naza <nafm@live.com.ar>
2019-04-02 20:01:12 -0300

    Fetcher construction

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,18 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from datetime import date, timedelta
+import os
+from time import sleep
 
-driver = webdriver.Chrome()
+
+def date_to_str(dt):
+    return '{}.{:0>2d}.{:0>2d}'.format(dt.year,dt.month,dt.day)
+
+
+c_options = webdriver.ChromeOptions()
+# c_options.headless = True
+c_options.add_argument('window-size=0x0')
+new_options = {'download.default_directory' : os.getcwd()}
+c_options.add_experimental_option('prefs', new_options)
+driver = webdriver.Chrome(chrome_options=c_options)
 driver.get('https://www.bcra.gob.ar/PublicacionesEstadisticas/Evolucion_moneda.asp')
 
-initial_date = '2019.03.06'
+years_back = 1
+today = date.today()
+initial_date = today - timedelta(days=365*years_back)
+
+# initial_date = '2019.03.17'
+
 date_select = Select(driver.find_element_by_name('Fecha'))
-date_select.select_by_value(initial_date)
+while True:
+    try:
+        date_select.select_by_value(date_to_str(initial_date))
+    except NoSuchElementException:
+        initial_date += timedelta(days=1)
+    else:
+        break
+
 
 currency_select = Select(driver.find_element_by_name('Moneda'))
 currency_select.select_by_value('2')
 
 see_button = driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div/form/div[3]/button')
 see_button.click()
+
+excel_file = driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div/a').click()
+sleep(2)
+driver.quit()
+
 
 
 
