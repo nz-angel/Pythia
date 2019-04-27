5d501ee79ed20893599f372855535adf96fe4f64
Naza <nafm@live.com.ar>
2019-04-27 19:26:03 -0300

    Fetcher construction

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,6 +3,8 @@ from time import sleep
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
+import warnings
+import os
 import csv
 
 
@@ -46,6 +48,11 @@ def collect_row_data(xpath, driver, collection):
 
 
 def build_csv(collection):
+    """
+    Writes the .csv file with the collected data
+    :param collection: list with the data dictionaries of each registered day
+    :type collection: list
+    """
     with open('data.csv', 'w+', newline="") as f:
         file_writer = csv.writer(f, dialect='excel', delimiter=';')
         file_writer.writerow(['Day', 'Month', 'Year', 'Value', 'Delta', 'Increase', 'Weekday', 'After Holiday'])
@@ -54,49 +61,130 @@ def build_csv(collection):
                                   elem['increase'], elem['weekday'], elem['after holiday']])
 
 
-c_options = webdriver.ChromeOptions()
-c_options.add_argument('headless')
-driver = webdriver.Chrome(chrome_options=c_options)
-driver.get('https://www.bcra.gob.ar/PublicacionesEstadisticas/Evolucion_moneda.asp')
-
-years_back = 0.5
-today = date.today()
-initial_date = today - timedelta(days=365*years_back+1)
-
-date_select = Select(driver.find_element_by_name('Fecha'))
-while True:
-    try:
-        date_select.select_by_value(date.strftime(initial_date, '%Y.%m.%d'))
-    except NoSuchElementException:
-        initial_date += timedelta(days=1)
+def update_data(end_date, path_to_csv):
+    """
+    Updates the data stored in an already existing data.csv . Reads the file's latest entry, extracts the date of the
+    corresponding day and adds the information of the days until end_date to the file. The function performs no
+    changes if the end date is prior to the last date recorded in the file.
+    :param end_date: the last day that wants to be recorded
+    :type end_date: date
+    :param path_to_csv: path to data.csv
+    :type path_to_csv: str
+    """
+    if end_date is None:
+        end_date = date.today()
     else:
-        break
+        end_date = datetime.strptime(end_date, '%d/%m/%Y')
 
-
-currency_select = Select(driver.find_element_by_name('Moneda'))
-currency_select.select_by_value('2')
-
-see_button = driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div/form/div[3]/button')
-see_button.click()
-sleep(1)
-data = []
-i = 2
-while True:
     try:
-        xp = '/html/body/div/div[2]/div/div/div/table/tbody/tr[{}]'.format(i)
-        data.append(collect_row_data(xp, driver, data))
-        i += 1
-        print(data[-1])
-    except NoSuchElementException:
-        break
-    finally:
-        if i >= 10:
+        with open(os.path.join(path_to_csv, 'data.csv'), 'r', newline='') as f:
+            f.seek(0, 2)
+            fsize = f.tell()
+            f.seek(fsize-60, 0)
+            last_entry = list(map(int, f.readlines()[-1].split(';')[:3]))
+            initial_date = date(last_entry[2], last_entry[1], last_entry[0]) + timedelta(days=1)
+    except FileNotFoundError:
+        raise FileNotFoundError('data.csv was not found. Generate the file or specify a correct path')
+    else:
+        if initial_date >= end_date:
+            warnings.warn('Last day recorded is posterior to end_date. No changes performed.')
+            return
+
+        new_data = parse_table(initial_date, end_date)
+        with open('data.csv', 'a', newline='') as f:
+            file_writer = csv.writer(f, dialect='excel', delimiter=';')
+            for elem in new_data:
+                file_writer.writerow([elem['day'], elem['month'], elem['year'], elem['value'], elem['delta'],
+                                      elem['increase'], elem['weekday'], elem['after holiday']])
+
+
+def parse_table(initial_date, end_date):
+    """
+    Selenium driver that parses the table found at the website of the Central Bank of Argentina. To access the
+    information, it's necessary to set an initial date. In the website, the exchange rate is informed from the initial
+    day to the latest bussiness day. If the end date happens to be a non-bussiness day, the information of the
+    following business day is recorded. Likewise, if the initial date is not a business day, the information is
+    recorded since the first business day following it.
+    :param initial_date: initial date from which data wants to be recorded
+    :type initial_date: date
+    :param end_date: last date to which data wants to be recorded
+    :type end_date: date
+    :return: list of dictionaries that represent the data obtained from each one of the table's rows
+    :rtype: list
+    """
+    c_options = webdriver.ChromeOptions()
+    c_options.add_argument('headless')
+    driver = webdriver.Chrome(chrome_options=c_options)
+    driver.get('https://www.bcra.gob.ar/PublicacionesEstadisticas/Evolucion_moneda.asp')
+
+    date_select = Select(driver.find_element_by_name('Fecha'))
+    while True:
+        try:
+            date_select.select_by_value(date.strftime(initial_date, '%Y.%m.%d'))
+        except NoSuchElementException:
+            initial_date += timedelta(days=1)
+        else:
             break
-driver.quit()
-build_csv(data)
+
+    currency_select = Select(driver.find_element_by_name('Moneda'))
+    currency_select.select_by_value('2')
+
+    see_button = driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div/form/div[3]/button')
+    see_button.click()
+    sleep(1)
+    data = []
+    i = 2
+    while True:
+        try:
+            xp = '/html/body/div/div[2]/div/div/div/table/tbody/tr[{}]'.format(i)
+            data.append(collect_row_data(xp, driver, data))
+            # print(data[-1])
+        except NoSuchElementException:
+            break
+        else:
+            last_date = driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div/table/tbody/tr[{}]/td['
+                                                     '1]'.format(i)).text
+            i += 1
+            if end_date <= datetime.strptime(last_date, '%d/%m/%Y').date():
+                break
+    driver.quit()
+    return data
 
 
+def main(years_back=None, initial_date=None, end_date=None, update=False, path_to_csv=''):
+    if update:
+        update_data(end_date, path_to_csv)
+    else:
+        # If no input is defined, the default is setting the initial date to one year ago
+        if all(a is None for a in [years_back, initial_date, end_date]):
+            years_back = 1
 
+        # years_back overrides other inputs if not None
+        if years_back:
+            end_date = date.today()
+            initial_date = end_date - timedelta(days=365*years_back+1)
+        else:
+            if initial_date is None and end_date is not None:
+                end_date = datetime.strptime(end_date, '%d/%m/%Y').date()
+                initial_date = end_date - timedelta(days=365+1)
+            elif initial_date is not None and end_date is None:
+                end_date = date.today()
+            else:
+                initial_date = datetime.strptime(initial_date, '%d/%m/%Y').date()
+                end_date = datetime.strptime(end_date, '%d/%m/%Y').date()
+                if initial_date >= end_date:
+                    warnings.warn('Initial date is posterior to end date. Setting the initial date to the day before '
+                                  'the end date')
+                    initial_date = end_date - timedelta(days=1)
+
+        data = parse_table(initial_date, end_date)
+        build_csv(data)
+
+
+if __name__ == '__main__':
+    main(years_back=6)
+
+    
 
 
 
